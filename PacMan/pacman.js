0b4106ed2fc27c2963e8d06845f3abb22b04b741
Marco Tejedor González <marco.tejedorg@gmail.com>
2016-04-10 20:05:38 +0200

    Changed the shape of the program. Starting with the player (pacMan).

--- PacMan/pacman.js
+++ PacMan/pacman.js
@@ -1,17 +1,25 @@
 function startGame ()
 {
     gameArea.create();
-    gameArea.map = MAP;
-    gameArea.drawScenary();
+    gameArea.init(MAP);
+    gameArea.draw();
 }
 
 var gameArea =
 {
-    gameCanvas: document.createElement("canvas"),
-    blockSize: {},     //x, y
-    map: [],
+    map:[],
+    blockSize: {},
+
+    init: function(map)
+    {
+        this.map = map;
+        scenary.init(this.gameCanvas, this.gameContext, this.blockSize, map);
+        //pacMan.init(this.gameCanvas, this.gameContext, this.blockSize, map);
+    },
+
     create: function ()
     {
+        this.gameCanvas = document.createElement("canvas");
         this.gameCanvas.id = "gameCanvas";
         this.gameCanvas.width = 342;
         this.gameCanvas.height = 550;
@@ -24,37 +32,71 @@ var gameArea =
         this.gameContext.clearRect(0, 0, this.gameCanvas.width, this.gameCanvas.height);
     },
 
+    draw: function ()
+    {
+        this.clear();
+        /* Refresh every time, just in case it has changed */
+        this.blockSize.width = this.gameCanvas.width / this.map[0].length;
+        this.blockSize.height = this.gameCanvas.height / this.map.length;
+
+        var line;
+        for (var y in this.map)
+        {
+            var line = this.map[y];
+            for (var x in line)
+            {
+                var item = line[x];
+                scenary.draw(item, x, y);
+            }
+        }
+    }
+}
+
+var scenary =
+{
+    init: function(canvas, context, blocksize, map)
+    {
+        this.canvas = canvas;
+        this.context = context;
+        this.blockSize = blocksize;
+        this.map = map;
+    },
+
+    update: function(map, blocksize = this.blockSize)
+    {
+        this.blockSize = blocksize;
+        this.map = map;
+    },
+
     drawRect: function (x, y, color, offsetx = 0, offsety = 0, width = this.blockSize.width, height = this.blockSize.height )
     {
-        this.gameContext.fillStyle = color;
+        this.context.fillStyle = color;
         x = x * this.blockSize.width + offsetx;
         y = y * this.blockSize.height + offsety;
-        this.gameContext.fillRect(x, y, width, height);
+        this.context.fillRect(x, y, width, height);
     },
 
     drawCircle: function (x, y, color, big)
     {
         y = parseInt(y);
         x = parseInt(x);
-        this.gameContext.fillStyle = color;
+        this.context.fillStyle = color;
         x = x * this.blockSize.width + this.blockSize.width/2;
         y = y * this.blockSize.height + this.blockSize.height/2;
         var size = Math.min(this.blockSize.width,this.blockSize.height)/6;
         if (big){size = 2*size};
-        this.gameContext.beginPath();
-        this.gameContext.arc(x, y, size, 0, 2*Math.PI);
-        this.gameContext.fill();
+        this.context.beginPath();
+        this.context.arc(x, y, size, 0, 2*Math.PI);
+        this.context.fill();
     },
 
-    isNotBorder: function (x,y)
-    {
+    isNotBorder: function (x,y) {
         var ans = 0<=y && y<this.map.length;
         ans = ans && 0<=x && x<this.map[0].length;
         return ans;
     },
 
-    drawWall: function (x, y)
-    {
+    drawWall: function (x, y) {
         x3 = this.blockSize.width / 3;
         y3 = this.blockSize.height / 3;
         y = parseInt(y);
@@ -91,8 +133,7 @@ var gameArea =
     },
 
     drawEmpty: function (x, y) { this.drawRect(x, y, "black") },
-    drawBlock: function (x, y) { this.drawWall(x, y) },
-    drawFruit: function (x, y) { this.drawRect(x, y, "red") },
+    drawFruit: function (x, y) { this.drawBiscuit(x,y,true)},
 
     draw: function (item, x, y)
     {
@@ -105,40 +146,62 @@ var gameArea =
                 this.drawBiscuit(x, y, false);
                 break;
             case 2:                         //Empty
-                //this.drawEmpty(x, y);
+                this.drawEmpty(x, y);
                 break;
             case 3:                         //Block
-                //this.drawBlock(x, y);
+                this.drawEmpty(x, y);
                 break;
             case 4:                         //Fruit
                 this.drawBiscuit(x, y, true);
                 break;
             case 5:
-                //sthis.drawFruit(x,y);
+                this.drawFruit(x,y);
                 break;
             default:
                 throw "Not contempled number in Map";
         }
+    }
+}
+
+var pacMan =
+{
+    x: 0,
+    y: 0,
+    open: true,
+    color: "yellow",
+
+    init: function(canvas, context, blocksize, map)
+    {
+        this.canvas = canvas;
+        this.context = context;
+        this.blockSize = blocksize;
+        this.map = map;
     },
 
-    drawScenary: function ()
+    update: function(map, blocksize = this.blockSize)
     {
-        this.clear();
-        /* Refresh every time, just in case it has changed */
-        this.blockSize.width = this.gameCanvas.width / this.map[0].length;
-        this.blockSize.height = this.gameCanvas.height / this.map.length;
+        this.blockSize = blocksize;
+        this.map = map;
+    },
 
-        var line;
-        for (var y in this.map)
+    draw: function()
+    {
+        y = parseInt(pacMan.y);
+        x = parseInt(pacMan.x);
+        this.context.fillStyle = pacMan.color;
+        x = x * this.blockSize.width + this.blockSize.width/2;
+        y = y * this.blockSize.height + this.blockSize.height/2;
+        var size = Math.min(this.blockSize.width,this.blockSize.height);
+        this.context.beginPath();
+        if (pacMan.close)
         {
-            var line = this.map[y];
-            for (var x in line)
-            {
-                var item = line[x];
-                this.draw(item, x, y);
-            }
+            this.context.arc(x, y, size, 0, 2*Math.PI);
+        } else {
+            this.context.arc(x, y, size, Math.PI/5, 9*Math.PI/5);
         }
-    }
+        this.context.fill();
+    },
+
 }
 
     WALL    = 0;
