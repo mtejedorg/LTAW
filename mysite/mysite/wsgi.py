115619a996df3b06769b9feb28c6bc653ba26fe7
mtejedorg <marco.bibi@gmail.com>
2016-05-07 19:45:25 +0200

    Doing django tutorial

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -1,14 +1,16 @@
 """
-WSGI config for djangop4 project.
+WSGI config for mysite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangop4.settings")
 
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
 application = get_wsgi_application()
