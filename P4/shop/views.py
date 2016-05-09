7892db41c4f718ae100a1ce47e7cf7b872d58173
mtejedorg <marco.bibi@gmail.com>
2016-05-09 20:24:41 +0200

    Working in views

--- P4/shop/views.py
+++ P4/shop/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
+
+from django.utils import timezone
+
+from .models import Genre, Item
 
 # Create your views here.
+def index(request):
+    genre_list = Genre.objects.order_by('-pub_date')[:4]
+    context = {'genre_list': genre_list}
+    return render(request, 'polls/index.html', context)
+
+class genreView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+
+class itemView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk = question_id)
+    try:
+        selected_choice = question.choice_set.get(pk = request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting from
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after succesfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
