6f08cc0c9211b1a934e7cfcffd02242ee4a1aba2
mtejedorg <marco.bibi@gmail.com>
2016-05-18 03:21:17 +0200

    Script and CSS are now external, and my server is implemented so that it can serve them (plus media)

--- WebSockets/index.js
+++ WebSockets/index.js
@@ -1,30 +1,26 @@
-var http = require('http').createServer(handler);
-var io = require('socket.io')(http);
+var handler = require('./myserver/handler')
+var server = require('http').createServer(handler.handler);
+var io = require('socket.io')(server);
 var fs = require('fs');
+var port = process.env.PORT || 3000;
 
-http.listen(3000);
+server.listen(port, function () {
+  console.log('Server listening at port ' + port);
+});
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
+var numUsers = 0;
+var users = {};
 
-    res.writeHead(200);
-    res.end(data);
-  });
-}
-
-io.on('connection', function (socket) {
+io.on('connection', function (socket, username) {
     console.log('a user conected');
     socket.on('disconnect', function(){
         console.log('user disconnected');
     });
-  //socket.emit('news', { hello: 'world' });
+
+    // When someone writes a message
     socket.on('chat message', function(msg){
         //socket.broadcast.emit('chat message', msg);
         io.emit('chat message', msg);
     });
+
 });
